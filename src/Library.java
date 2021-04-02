65c30e651ee65fe11abdeccf20673e529ac5383c
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-04-03 00:33:05 +0200

    init commit

---
+++ src/Library.java
@@ -0,0 +1,20 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "biblioteka v0.1";
+
+        String title = "W Pustyni i w puszczy";
+        String author = "Henryk sienkiewicz";
+        int releaseDate = 2010;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "9788373271890";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece");
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
