b2b1a9fbc7e983360a66147f9f864b228ef181a4
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-04-05 11:14:25 +0200

    added book class //a new class to represent a single book

--- src/Library.java
+++ src/Library.java
@@ -1,20 +1,25 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "biblioteka v0.1";
+        final String appName = "Biblioteka v0.2";
 
-        String title = "W Pustyni i w puszczy";
-        String author = "Henryk sienkiewicz";
-        int releaseDate = 2010;
-        int pages = 296;
-        String publisher = "Greg";
-        String isbn = "9788373271890";
+        Book book1 = new Book();
+
+        book1.title = "W Pustyni i w puszczy";
+        book1.author = "Henryk sienkiewicz";
+        book1.releaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "9788373271890";
+
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece");
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
+
     }
 }
