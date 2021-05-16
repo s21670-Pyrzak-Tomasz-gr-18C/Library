5639d34549f290234925e4b5490ad3f1f47fb35b
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-05-16 13:31:58 +0200

    constructors added Java beans were used

--- src/Library.java
+++ src/Library.java
@@ -1,25 +1,21 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
 
-        Book book1 = new Book();
+            final String appName = "Biblioteka v0.5";
 
-        book1.title = "W Pustyni i w puszczy";
-        book1.author = "Henryk sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890";
+            Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg",
+                    "9788373271890");
+            Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", " Joshua Bloch", 2009,
+                    352, "Helion", "9788324620845");
+            Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+                    "Bert Bates, Katherine Sierra", 2008, 851, "McGraw-Hill Osborne Media");
 
-
-        System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+            System.out.println(appName);
+            System.out.println("Książki dostępne w bibliotece:");
+            book1.printInfo();
+            book2.printInfo();
+            book3.printInfo();
+        }
 
     }
-}
+
