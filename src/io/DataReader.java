6bff7b66d380ca721bbde179b104b56af6ccd21c
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-05-28 09:42:43 +0200

    v0.9 extracting program logic into the LibraryControl class

--- src/io/DataReader.java
+++ src/io/DataReader.java
@@ -7,27 +7,30 @@ import java.util.Scanner;
 public class DataReader {
     private Scanner sc = new Scanner(System.in);
 
-public Book readAndCreateBook(){
-    System.out.println("Tytuł");
-    String title = sc.nextLine();
-    System.out.println("Autor");
-    String author = sc.nextLine();
-    System.out.println("Wydawnictwo");
-    String publisher = sc.nextLine();
-    System.out.println("ISBN:");
-    String isbn = sc.nextLine();
-    System.out.println("Rok wydania:");
-    int releaseDate = sc.nextInt();
-    sc.nextLine();
-    System.out.println("Liczba stron:");
-    int pages = sc.nextInt();
-    sc.nextLine();
-    return new Book(title,author,releaseDate,pages,publisher,isbn);
-}
+    public void close() {
+        sc.close();
+    }
 
-public void close(){
-    sc.close();
-}
+    public int getInt() {
+        int number = sc.nextInt();
+        sc.nextLine();
+        return number;
+    }
 
+    public Book readAndCreateBook() {
+        System.out.println("Tytuł: ");
+        String title = sc.nextLine();
+        System.out.println("Autor: ");
+        String author = sc.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int releaseDate = getInt();
+        System.out.println("Ilość stron: ");
+        int pages = getInt();
 
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
 }
